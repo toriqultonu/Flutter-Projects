c17b15365f2e0f284fd025ed1e2af9b5ca79bc82
toriqultonu <toriqultonu22@gmail.com>
2021-04-15 09:34:52 -0700

    Http Package imported
    http package imported successfully to send http request and get response.

--- weatherbot/lib/screens/loading_screen.dart
+++ weatherbot/lib/screens/loading_screen.dart
@@ -1,7 +1,9 @@
+import 'dart:convert';
 import 'package:flutter/material.dart';
-import 'package:geolocator/geolocator.dart';
-import 'package:http/http.dart';
+import 'package:http/http.dart' as http;
 import 'package:weatherbot/services/location.dart';
+
+const apiKey = '07b2b0174e0f21f92ef5985a232e6c7d';
 
 class LoadingScreen extends StatefulWidget {
   @override
@@ -10,17 +12,26 @@ class LoadingScreen extends StatefulWidget {
 
 class _LoadingScreenState extends State<LoadingScreen> {
 
+  double latitude;
+  double longitude;
+
   void getLocation() async {
      Location location = Location();
 
      await location.getCurrentLocation();
-     print(location.latitude);
-     print(location.longitude);
+     latitude = location.latitude;
+     longitude = location.longitude;
+
+
   }
 
-  // void  getData(){
-  //   get(url)
-  // }
+  void  getData() async {
+    http.Response response = await http.get('https://api.openweathermap.org/data/2.5/weather?lat=$latitude&lon=$longitude&appid=$apiKey');
+    print(longitude);
+    print(latitude);
+    print(response.statusCode);
+
+  }
 
   @override
   void initState() {
@@ -33,12 +44,14 @@ class _LoadingScreenState extends State<LoadingScreen> {
 
   @override
   Widget build(BuildContext context) {
+
     return Scaffold(
       body: Center(
         child: RaisedButton(
           onPressed: () {
             //Get the current location
             //getLocation();
+            getData();
           },
           child: Text('Get Location'),
         ),

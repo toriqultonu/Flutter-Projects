5932aaabef97567ef38cae3e4d1047f9e291f640
toriqultonu <toriqultonu22@gmail.com>
2021-04-15 10:58:38 -0700

    Network.dart file is created
    Network.dart file is created with NetworkHelper class to separate the getting of JSON decoded data as a response to http request

--- weatherbot/lib/screens/loading_screen.dart
+++ weatherbot/lib/screens/loading_screen.dart
@@ -2,6 +2,7 @@ import 'dart:convert';
 import 'package:flutter/material.dart';
 import 'package:http/http.dart' as http;
 import 'package:weatherbot/services/location.dart';
+import 'package:weatherbot/services/networking.dart';
 
 const apiKey = '07b2b0174e0f21f92ef5985a232e6c7d';
 
@@ -15,50 +16,32 @@ class _LoadingScreenState extends State<LoadingScreen> {
   double latitude;
   double longitude;
 
-  void getLocation() async {
+  void getLocationData() async {
      Location location = Location();
 
      await location.getCurrentLocation();
      latitude = location.latitude;
      longitude = location.longitude;
 
+     NetworkHelper networkHelper = NetworkHelper('https://api.openweathermap.org/data/2.5/weather?lat=$latitude&lon=$longitude&appid=$apiKey');
 
-  }
+     var weatherData = await networkHelper.getData();
 
-  void  getData() async {
-    http.Response response = await http.get('https://api.openweathermap.org/data/2.5/weather?lat=$latitude&lon=$longitude&appid=$apiKey');
-    // print(longitude);
-    // print(latitude);
-    // print(response.statusCode);
+     double temperature = weatherData['main']['temp'];
+     int condition = weatherData['weather'][0]['id'];
+     String cityName = weatherData['name'];
 
-    if(response.statusCode == 200){
-      String data = response.body;
-
-      var decodedData = jsonDecode(data);
-
-      double temperature = decodedData['main']['temp'];
-      int condition = decodedData['weather'][0]['id'];
-      String cityName = decodedData['name'];
-
-      print(temperature);
-      print(condition);
-      print(cityName);
-    }
-    else{
-      print(response.statusCode);
-    }
-
-
+     print(temperature);
+     print(condition);
+     print(cityName);
   }
 
   @override
   void initState() {
 
     super.initState();
-    getLocation();
+    getLocationData();
   }
-
-
 
   @override
   Widget build(BuildContext context) {

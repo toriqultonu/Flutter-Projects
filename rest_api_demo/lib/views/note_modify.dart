eb711ce67ae94b0a7a3e36bd9e53169f6eddd3ea
toriqultonu <toriqultonu22@gmail.com>
2021-04-17 00:24:21 -0700

    UI updated with note modify
    note_modify.dart file added to modify or create a note

--- rest_api_demo/lib/views/note_modify.dart
+++ rest_api_demo/lib/views/note_modify.dart
@@ -5,8 +5,34 @@ class NoteModify extends StatelessWidget {
   Widget build(BuildContext context) {
     return Scaffold(
       appBar: AppBar(title: Text('Create note'),),
-      body: Container(
+      body: Padding(
+        padding: const EdgeInsets.all(12.0),
+        child: Column(
+          children: [
+            TextField(decoration: InputDecoration(
+              hintText: 'Note title'
+            ),),
+            SizedBox(height: 8,),
 
+            TextField(
+              decoration: InputDecoration(
+                hintText: 'Note content'
+              ),
+            ),
+            SizedBox(height: 18,),
+            SizedBox(
+              width: double.infinity,
+              child: RaisedButton(
+                child: Text('Submit',style: TextStyle(color: Colors.white,fontSize: 18),),
+                color: Theme.of(context).primaryColor,
+                onPressed: () {
+                  Navigator.of(context).pop();
+                },
+              ),
+            )
+          ],
+
+        ),
       )
     );
   }

848d1db31bc3e111e22a0bdbefd09cdacf14f565
toriqultonu <toriqultonu22@gmail.com>
2021-04-18 05:03:07 -0700

    Delete functionality added
    Added some functionality to the note list including delete function using swipe left to right

--- rest_api_demo/lib/views/note_list.dart
+++ rest_api_demo/lib/views/note_list.dart
@@ -1,5 +1,7 @@
+import 'package:flutter/foundation.dart';
 import 'package:flutter/material.dart';
 import 'package:rest_api_demo/models/note_for_listing.dart';
+import 'package:rest_api_demo/views/note_delete.dart';
 import 'package:rest_api_demo/views/note_modify.dart';
 
 class NoteList extends StatelessWidget {
@@ -24,14 +26,36 @@ class NoteList extends StatelessWidget {
         child: Icon(Icons.add),
       ),
       body: ListView.separated(
-        separatorBuilder: (_, __) => Divider(height: 1, color: Colors.green,),
+        separatorBuilder: (_, __) => Divider( color: Colors.green,),
         itemBuilder: (_, index){
-          return ListTile(
-            title: Text(
-              notes[index].noteTitle,
-              style: TextStyle(color: Theme.of(context).primaryColor),
+          return Dismissible(
+            key: ValueKey(notes[index].noteID),
+            direction: DismissDirection.startToEnd,
+            onDismissed: (direction){
+
+            },
+            confirmDismiss: (direction) async {
+              final result = await showDialog(
+                  context: context,
+                  builder: (_) => NoteDelete()
+              );
+              return result;
+            },
+            background: Container(
+              color: Colors.red,
+              padding: EdgeInsets.only(left: 16),
+              child: Align(child: Icon(Icons.delete, color: Colors.white), alignment: Alignment.centerLeft,),
             ),
-            subtitle: Text('Last edited on ${notes[index].latestEditDateTime}'),
+            child: ListTile(
+              title: Text(
+                notes[index].noteTitle,
+                style: TextStyle(color: Theme.of(context).primaryColor),
+              ),
+              subtitle: Text('Last edited on ${notes[index].latestEditDateTime}'),
+              onTap: (){
+                Navigator.of(context).push(MaterialPageRoute(builder: (_) => NoteModify(noteID: notes[index].noteID)));
+              },
+            ),
           );
         },
         itemCount: notes.length,
--- rest_api_demo/lib/views/note_modify.dart
+++ rest_api_demo/lib/views/note_modify.dart
@@ -1,10 +1,16 @@
 import 'package:flutter/material.dart';
 
 class NoteModify extends StatelessWidget {
+
+  final String noteID;
+  bool get isEditing => noteID != null;
+
+  NoteModify({this.noteID});
+
   @override
   Widget build(BuildContext context) {
     return Scaffold(
-      appBar: AppBar(title: Text('Create note'),),
+      appBar: AppBar(title: Text(isEditing ? 'Edit note':'Create note'),),
       body: Padding(
         padding: const EdgeInsets.all(12.0),
         child: Column(

f42bffe83d10e2b8143b817485545de54f7a47d7
toriqultonu <toriqultonu22@gmail.com>
2021-05-20 09:30:55 -0700

    dynamic appbar created

--- tts_network/lib/main.dart
+++ tts_network/lib/main.dart
@@ -12,7 +12,7 @@ class MyApp extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     return MaterialApp(
-      title: 'FlutterShare',
+      title: 'tts_network',
       debugShowCheckedModeBanner: false,
       theme: ThemeData(
         primaryColor: Colors.green,

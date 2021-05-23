b671dfb829a66b3665fc0b626b35d681de5d5493
toriqultonu <toriqultonu22@gmail.com>
2021-05-22 23:43:03 -0700

    section 5 completed
    create user completely done with authentication and validation

--- tts_network/lib/pages/create_account.dart
+++ tts_network/lib/pages/create_account.dart
@@ -1,3 +1,5 @@
+import 'dart:async';
+
 import 'package:flutter/material.dart';
 import 'package:tts_network/widgets/header.dart';
 
@@ -8,18 +10,28 @@ class CreateAccount extends StatefulWidget {
 
 class _CreateAccountState extends State<CreateAccount> {
 
+  final _scaffoldKey =  GlobalKey<ScaffoldState>();
   final _formKey = GlobalKey<FormState>();
   String username;
 
   submit(){
-    _formKey.currentState.save();
-    Navigator.pop(context, username);
+    final form = _formKey.currentState;
+    if(form.validate()){
+      form.save();
+      SnackBar snackBar = SnackBar(content: Text("Welcome $username!"));
+      _scaffoldKey.currentState.showSnackBar(snackBar);
+      Timer(Duration(seconds: 2),(){
+        Navigator.pop(context, username);
+      });
+
+    }
   }
 
   @override
   Widget build(BuildContext parentContext) {
     return Scaffold(
-      appBar: header(context, titleText: "Set up your profile"),
+      key: _scaffoldKey,
+      appBar: header(context, titleText: "Set up your profile", removeBackButton: true),
       body: ListView(
         children: [
           Container(
@@ -35,7 +47,19 @@ class _CreateAccountState extends State<CreateAccount> {
                 child: Container(
                   child: Form(
                     key: _formKey,
+                    autovalidate: true,
                     child: TextFormField(
+                      validator: (value){
+                        if(value.trim().length < 3 || value.isEmpty){
+                          return "Username too short!";
+                        }
+                        else if(value.trim().length > 12){
+                          return "Username too long";
+                        }
+                        else{
+                          return null;
+                        }
+                      },
                       onSaved: (value) => username = value,
                       decoration: InputDecoration(
                         border: OutlineInputBorder(),

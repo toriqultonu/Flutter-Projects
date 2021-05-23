a33d8c2421b7d8c51242ea50589a2f8110e57232
toriqultonu <toriqultonu22@gmail.com>
2021-05-23 08:22:09 -0700

    search field added

--- tts_network/lib/pages/search.dart
+++ tts_network/lib/pages/search.dart
@@ -1,14 +1,56 @@
 import 'package:flutter/material.dart';
+import 'package:flutter_svg/flutter_svg.dart';
 
 class Search extends StatefulWidget {
   @override
   _SearchState createState() => _SearchState();
 }
 
+AppBar buildSearchField() {
+  return AppBar(
+    backgroundColor: Colors.white,
+    title: TextFormField(
+      decoration: InputDecoration(
+          hintText: "Search for a user..",
+          filled: true,
+          prefixIcon: Icon(
+            Icons.account_box,
+            size: 28.0,
+          ),
+          suffixIcon: IconButton(
+            icon: Icon(Icons.clear),
+            onPressed: () => print("Cleared"),
+          )),
+    ),
+  );
+}
+
+buildNoContent() {
+  return Container(
+    child: Center(
+      child: ListView(
+        shrinkWrap: true,
+        children: [
+          SvgPicture.asset(
+            'assets/images/search.svg',
+            height: 300,
+          ),
+          Text("Find Users", textAlign: TextAlign.center, style: TextStyle(color: Colors.white, fontStyle: FontStyle.italic, fontWeight: FontWeight.w600, fontSize: 60,),
+          )
+        ],
+      ),
+    ),
+  );
+}
+
 class _SearchState extends State<Search> {
   @override
   Widget build(BuildContext context) {
-    return Text('Search');
+    return Scaffold(
+      backgroundColor: Theme.of(context).primaryColor.withOpacity(0.8),
+      appBar: buildSearchField(),
+      body: buildNoContent(),
+    );
   }
 }
 
